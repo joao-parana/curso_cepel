2689f1c080f6af49ca1334a6b1b9d69711207ce2
João Antonio Ferreira <joao.parana@gmail.com>
2022-10-05 14:23:52 -0300

    testing git hook

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ from cepel import cepel
 class MyTest(unittest.TestCase):
     def test(self):
         print('testando a classe MyTest agora com hook pre-commit local')
-        print('fase 3')
-        self.assertEqual(cepel.soma(1, 2), 4)
+        self.assertEqual(cepel.soma(1, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
-
